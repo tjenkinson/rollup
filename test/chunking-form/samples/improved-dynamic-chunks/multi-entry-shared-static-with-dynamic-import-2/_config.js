678802997c1d3ef65172542127ad5956092cdba6
Tom Jenkinson <tom@tjenkinson.me>
2020-01-16 20:36:10 +0000

    new tests

---
+++ test/chunking-form/samples/improved-dynamic-chunks/multi-entry-shared-static-with-dynamic-import-2/_config.js
@@ -0,0 +1,10 @@
+// why `import './generated-dep.js';`?
+
+module.exports = {
+	solo: true, // TODO the result from this one doesn't seem right
+	description:
+		'avoids chunks for always loaded dependencies if multiple entry points with different dependencies have dynamic imports',
+	options: {
+		input: ['main1', 'main2', 'main3']
+	}
+};
---
+++ test/chunking-form/samples/improved-dynamic-chunks/multi-entry-shared-static-with-dynamic-import/_config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	solo: true,
+	description:
+		'avoids chunks for always loaded dependencies if multiple entry points with different dependencies have dynamic imports',
+	options: {
+		input: ['main1', 'main2']
+	}
+};
